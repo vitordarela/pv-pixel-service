84f7494e177d98d9f51fdd052fa790a693ce9eab
vitor.darela <vitor.darela@FFBRW5CD307344X>
2024-02-22 09:20:53 -0300

    Add description about Referer HTTP Header

--- Presentation.Api/Program.cs
+++ Presentation.Api/Program.cs
@@ -16,6 +16,7 @@ var app = builder.Build();
 
 app.MapGet("/track", async (HttpContext context, ILogger <Program> logger, ITrackingService trackingService) =>
 {
+    // In the documentation it says Referrer but the default HTTP Header is Referer, I identified it as if it were a typo and left the correct default. 
     var referer = context.Request.Headers["Referer"].ToString();
     var userAgent = context.Request.Headers["User-Agent"].ToString();
     var ipAddress = context.Request.Headers["x-forwarded-for"].ToString();
